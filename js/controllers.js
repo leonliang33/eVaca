b42611e8c1977c847cced229c152ac847f3cc271
leonliang33 <llian008@fiu.edu>
2016-06-29 17:22:22 -0400

    eVaca initial commit

---
+++ js/controllers.js
@@ -0,0 +1,46 @@
+angular.module('app.controllers', [])
+        
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('eVacaCtrl', function($scope) {
+
+})
+   
+.controller('historyCtrl', function($scope) {
+
+})
+   
+.controller('thingsToDoCtrl', function($scope) {
+
+})
+   
+.controller('signUpCtrl', function($scope) {
+
+})
+   
+.controller('plannerCtrl', function($scope) {
+
+})
+   
+.controller('verifcationCodeCtrl', function($scope) {
+
+})
+   
+.controller('resetPasswordCtrl', function($scope) {
+
+})
+   
+.controller('locationsCtrl', function($scope) {
+
+})
+   
+.controller('newPasswordCtrl', function($scope) {
+
+})
+   
+.controller('accountPreferencesCtrl', function($scope) {
+
+})
+ 
---
+++ js/routes.js
@@ -0,0 +1,105 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('tabsController.eVaca', {
+    url: '/main',
+    views: {
+      'tab4': {
+        templateUrl: 'templates/eVaca.html',
+        controller: 'eVacaCtrl'
+      }
+    }
+  })
+
+  .state('history', {
+    url: '/History',
+    templateUrl: 'templates/history.html',
+    controller: 'historyCtrl'
+  })
+
+  .state('tabsController.thingsToDo', {
+    url: '/events',
+    views: {
+      'tab4': {
+        templateUrl: 'templates/thingsToDo.html',
+        controller: 'thingsToDoCtrl'
+      }
+    }
+  })
+
+  .state('signUp', {
+    url: '/signup',
+    templateUrl: 'templates/signUp.html',
+    controller: 'signUpCtrl'
+  })
+
+  .state('tabsController.planner', {
+    url: '/planner',
+    views: {
+      'tab4': {
+        templateUrl: 'templates/planner.html',
+        controller: 'plannerCtrl'
+      }
+    }
+  })
+
+  .state('verifcationCode', {
+    url: '/verfication',
+    templateUrl: 'templates/verifcationCode.html',
+    controller: 'verifcationCodeCtrl'
+  })
+
+  .state('resetPassword', {
+    url: '/reset_request',
+    templateUrl: 'templates/resetPassword.html',
+    controller: 'resetPasswordCtrl'
+  })
+
+  .state('tabsController.locations', {
+    url: '/Locations',
+    views: {
+      'tab4': {
+        templateUrl: 'templates/locations.html',
+        controller: 'locationsCtrl'
+      }
+    }
+  })
+
+  .state('newPassword', {
+    url: '/pass_update',
+    templateUrl: 'templates/newPassword.html',
+    controller: 'newPasswordCtrl'
+  })
+
+  .state('accountPreferences', {
+    url: '/Accounts',
+    templateUrl: 'templates/accountPreferences.html',
+    controller: 'accountPreferencesCtrl'
+  })
+
+$urlRouterProvider.otherwise('/login')
+
+  
+
+});
