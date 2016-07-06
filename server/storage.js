40b554366b67fc40d09cd998fc520e1132f22fdf
lpere371 <lpere371@fiu.edu>
2016-07-06 18:14:46 -0400

    Functions added and connection method implemented

--- server/storage.js
+++ server/storage.js
@@ -3,7 +3,7 @@
 	Author:         Leon Liang
 	Created:        6/15/2016
 	For Version:		1.0x
-	Modifications:
+	Modifications:	Functions added. Connection to database implemented.
 	Summary:
           Storage javascript.
           All database work should be here :: storing and retrieving of information
@@ -13,7 +13,7 @@
 //Define Modules Requirement Here
 //Calls application level module
 var application = require("./application.js")
-var mongodb = require("mongodb").MongoClient;
+var mongoose = require('mongoose');
 
 
 
@@ -26,14 +26,24 @@ var mongodb = require("mongodb").MongoClient;
 
 /*********************************Export Modules******************************/
 
-//Login export :: Verify correct username and password
+//Verify correct username and password
 exports.login_verification = function login(usr,pass){
-
+	
 }
 
-//Modify Password
+//Modify password
 exports.modify_pass = function modPass(){
+	
+}
 
+//Modify email
+exports.modify_email = function modEmail(){
+	
+}
+
+//Modify username
+exports.modify_usrname = function modUsrname(){
+	
 }
 /*****************************************************************************/
 
@@ -41,6 +51,26 @@ exports.modify_pass = function modPass(){
 
 
 //**************************** Function Definitions ****************************
+
+
+/** ****************************************************************************
+  *             connectToDatabase()
+  *             Function to establish the connection to the dababase. It returns
+  *				true if the connection was successful, or false if unsuccessful.
+  * @param      {none} None
+  * @returns    boolean true or false
+  *****************************************************************************/
+  function connectToDatabase(){
+	  mongoose.connect('mongodb://localhost/evacadb');
+	  var db = mongoose.connection;
+	  db.on('error', console.error.bind(console, 'connection error:'));
+	  db.once('open', function() {
+		  console.log("Connection to database successful");
+		  return true;
+	  });
+	  console.log("Connection to database unsuccessful");
+	  return false;
+  }
 
 /** ****************************************************************************
   *             returnSuccess()
@@ -50,16 +80,16 @@ exports.modify_pass = function modPass(){
   *****************************************************************************/
 
   function returnSuccess(){
-
-       return true;
+	  
+      return true;
   }
 
-  /** ****************************************************************************
-    *             returnSuccess()
-    *             Return Account Params
-    * @param      None
-    * @returns    None
-    *****************************************************************************/
-    function getAccount(){
+/** ****************************************************************************
+  *             getAccount()
+  *             Return Account Params
+  * @param      
+  * @returns    
+  *****************************************************************************/
+  function getAccount(){
 
-    }
+  }
