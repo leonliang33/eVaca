009883655c20a87afccd82296eb977da4bd05f1f
lpere371 <lpere371@fiu.edu>
2016-07-11 15:33:57 -0400

    Functions added and connection to database moved to server.js

--- server/storage.js
+++ server/storage.js
@@ -3,7 +3,7 @@
 	Author:         Leon Liang
 	Created:        6/15/2016
 	For Version:		1.0x
-	Modifications:	Functions added. Connection to database implemented.
+	Modifications:	Functions added and connection to database moved to server.js.
 	Summary:
           Storage javascript.
           All database work should be here :: storing and retrieving of information
@@ -14,7 +14,7 @@
 //Calls application level module
 var application = require("./application.js");
 var mongoose = require('mongoose');
-var user_model = require('./models/user.model.js');
+
 
 
 //******************************* Global Variables *****************************
@@ -25,66 +25,80 @@ var db_url = 'mongodb://localhost/evacadb'
 
 
 /*********************************Export Modules******************************/
-/*These are functions that can be called outside the class*/
 
-//Authentication
-exports.auth = function auth(){
-
+//Verify correct username and password
+exports.login_verification = function login(usr, pass){
+	
 }
 
-//Update User
-exports.updateUser = function updateUser(email){
-
+//Modify password
+exports.modify_pass = function modPass(){
+	
 }
 
-exports.store = function store(User){
-
+//Modify email
+exports.modify_email = function modEmail(){
+	
 }
 
-exports.retrieve = function retrieve(email){
+//Modify username
+exports.modify_username = function modUsername(){
+	
+}
 
+//Function to insert one user in the database
+exports.insert_user = function insertUser(usr, pass, email, sec_q, sec_a){
+	
+}
+
+//Function used to delete a user 
+exports.delete_user = function deleteUser(email){
+	
+}
+
+//Function to add a planner to a given user with certain 
+exports.add_planner = function addPlanner(email_fk, city_fk, idPlanner, isActive){
+	
+}
+
+//Function to add the preferences to a planner
+exports.add_preference = function addPreference(city, occassion, age_appr, leave_date, return_date, ideal_opt, budget){
+	
+}
+
+//Adds an event to a specific planner
+exports.add_event = function addEvent(idPlanner_fk, name, date, time, cost){
+	
+}
+
+//Function used to update the planner
+exports.updatePlanner = function updatePlanner(email_fk, city_fk, idPlanner, isActive){
+	
+}
+
+//Function to update the preferences of a user
+exports.updatePreferences(city, occassion, age_appr, leave_date, return_date, ideal_opt, budget){
+	
+}
+
+//Function to get all the events of a user
+exports.get_allevents = function getAllEvents(email_fk){
+	
+}
+
+//Function to get all the events of a user for a given plannerm
+exports.get_events_planner = function getEventsByPlanner(email_fk, idPlanner_fk){
+	
+}
+
+//Function to get all the planners of a user
+exports.get_user_planners = function getUserPlanners(email_fk){
+	
 }
 /*****************************************************************************/
 
 
-
-
 //**************************** Function Definitions ****************************
-
-
-/** ****************************************************************************
-  *             connectToDatabase()
-  *             Function to establish the connection to the dababase. It returns
-  *				true if the connection was successful, or false if unsuccessful.
-  *
-  * @param      {none} None
-  * @returns    boolean true or false
-  *****************************************************************************/
-  function connectToDatabase(){
-
-	  mongoose.connect(db_url);
-	  var db = mongoose.connection;
-
-	  db.on('error', function(err){
-		  console.log("Connection to database unsuccessful.\n" + err);
-		  return false;
-	  });
-
-	  db.on('connected', function() {
-		  console.log("Connection to database successful.");
-		  return true;
-	  });
-
-	  db.on('disconnected', function () {
-		  console.log('Disconected from the database.');
-		  return false;
-	  });
-
-	  console.log("Connection to database unsuccessful.");
-	  return false;
-  }
-
-
 
 /** ****************************************************************************
   *             returnSuccess()
@@ -92,18 +106,17 @@ exports.retrieve = function retrieve(email){
   * @param      {none} None
   * @returns    boolean true or false
   *****************************************************************************/
-  function returnSuccess(){
 
+  function returnSuccess(){
+	  
       return true;
   }
-
-
 
 /** ****************************************************************************
   *             getAccount()
   *             Return Account Params
-  * @param
-  * @returns    JSON file with account details
+  * @param      
+  * @returns    
   *****************************************************************************/
   function getAccount(){
 
